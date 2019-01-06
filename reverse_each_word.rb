6848ad95baf0887972990a293d45a59bb7785bb0
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 02:57:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  reverse_words = []
   sentence_array.collect{|word| word.reverse}
-  reverse_words.join(' ')
+  sentence_array.join(' ')
 end
